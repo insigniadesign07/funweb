8c22eaf0e76b8be9aa228b964ab3af1d09303f28
Ahmad Raza <picmaticgraphicss@gmail.com>
2024-10-18 15:53:58 +0500

    connect mongoDB

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB();
+/*
+(async()=>{
+    try {
+      await  mongoose.connect(`${process.env.MONGODB_URI}/
+        ${DB_NAME}`)
+        app.on("error:" , (error)=>{
+            console.log("ERROR: " , error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is Running on ${process.env.PORT}`)
+        })
+
+    } catch (error) {
+        console.error("error: " , error)
+        throw err
+    }
+})() */
